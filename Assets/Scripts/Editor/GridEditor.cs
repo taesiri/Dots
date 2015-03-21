23d38dc81f515aa6bca8fd0078161383475c382c
Taesiri <taesiri@live.com>
2015-03-22 01:15:08 +0430

    Backup!

---
+++ Assets/Scripts/Editor/GridEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor
+{
+    [CustomEditor(typeof (Grid))]
+    public class GridEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Create Grid"))
+            {
+                CreateGrid();
+            }
+            if (GUILayout.Button("Delete Childern"))
+            {
+                DeleteChildern();
+            }
+        }
+
+        private void CreateGrid()
+        {
+            var grid = (Grid) target;
+
+            for (var i = 0; i < grid.Width; i++)
+            {
+                for (var j = 0; j < grid.Height; j++)
+                {
+                    var newDot = (GameObject) Instantiate(grid.CellObject, new Vector3(grid.OffsetX + i*grid.Padding, grid.OffsetZ + j*grid.Padding, 0), Quaternion.identity);
+                    newDot.transform.parent = grid.transform;
+                }
+            }
+        }
+
+        private void DeleteChildern()
+        {
+            var grid = (Grid) target;
+            var childs = new Transform[grid.transform.childCount];
+
+            for (var i = 0; i < grid.transform.childCount; i++)
+            {
+                childs[i] = grid.transform.GetChild(i);
+            }
+            for (var i = 0; i < childs.Length; i++)
+            {
+                DestroyImmediate(childs[i].gameObject);
+            }
+        }
+    }
+}
